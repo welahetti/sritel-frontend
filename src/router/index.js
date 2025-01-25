f301b4890f30f714906b7d8acf6cbdff1a15300f
welahetti <pgdharshika@gmail.com>
2025-01-25 15:40:43 +0530

    commit dashboard route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'; // For Vue 3
 import BillView from '../components/BillView.vue';
 import PaymentView from '../components/PaymentView.vue';
+import DashboardView from '../components/DashboardView.vue';
 
 
 const routes = [
   { path: '/bills', component: BillView },
   // Define other routes as necessary
   { path: '/payments/:id', component: PaymentView},
+  { path: '/dashboard', component: DashboardView},
   
 ];
 
